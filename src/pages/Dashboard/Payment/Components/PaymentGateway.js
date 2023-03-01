22682f31f6b02df06d0fa807ebba87e41ce74d70
carloshnp <carloshnpwork@gmail.com>
2023-03-01 17:18:12 -0300

    feat: Payment components + PaymentGateway page

---
+++ src/pages/Dashboard/Payment/Components/PaymentGateway.js
@@ -0,0 +1,16 @@
+export default function PaymentGateway() {
+  return {
+    /* 
+      Payment Container
+        Information Container 
+          Card Information Container
+          Form Container
+            Card Number
+            Name
+            Sensitive Card Info
+              Valid Thru 
+              CVC
+        Confirm Button
+    */
+  };
+}
