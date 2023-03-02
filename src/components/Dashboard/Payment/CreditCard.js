53b384ab52b5ca473bf2e093f3da0c756311224f
carloshnp <carloshnpwork@gmail.com>
2023-03-02 11:20:43 -0300

    feat: form input change, focus and submit

--- src/components/Dashboard/Payment/CreditCard.js
+++ src/components/Dashboard/Payment/CreditCard.js
@@ -9,6 +9,7 @@ const PaymentForm = () => {
     cvc: '',
     name: '',
     focus: '',
+    formData: null
   });
 
   const handleInputChange = (evt) => {
@@ -21,19 +22,68 @@ const PaymentForm = () => {
     setState((prev) => ({ ...prev, focus: evt.target.name }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = [...e.target.elements]
+      .filter(d => d.name)
+      .reduce((acc, d) => {
+        acc[d.name] = d.value;
+        return acc;
+      }, {});
+    setState({
+      number: '',
+      expiry: '',
+      cvc: '',
+      name: '',
+      focus: '',
+      formData: null
+    });
+    console.log(state);
+  };
+
   return (
     <div id="PaymentForm">
       <Cards number={state.number} expiry={state.expiry} cvc={state.cvc} name={state.name} focused={state.focus} />
-      <form>
+      <form onSubmit={handleSubmit} >
         <input
           type="number"
           name="number"
           placeholder="Card Number"
+          pattern='[\d| ]{16,22}'
+          required
           value={state.number}
           onChange={handleInputChange}
           onFocus={handleInputFocus}
         />
-        ...
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          required
+          value={state.name}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type='tel'
+          name='expiry'
+          placeholder='Valid Thru'
+          pattern='\d\d/\d\d'
+          required
+          value={state.expiry}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type='tel'
+          name='cvc'
+          placeholder='CVC'
+          pattern='\d{3,4}'
+          required
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
