6aa1d93a64d804d40449cf1c0f5a9f70907f1bb9
carloshnp <carloshnpwork@gmail.com>
2023-03-02 05:17:52 -0300

    feat: React Credit Cards package usage

--- src/pages/Dashboard/Payment/Components/PaymentGateway.js
+++ src/pages/Dashboard/Payment/Components/PaymentGateway.js
@@ -1,20 +1,12 @@
 import styled from 'styled-components';
+import PaymentForm from '../../../../components/Dashboard/Payment/CreditCard';
 
 export default function PaymentGateway() {
   return (
     <PaymentContainer>
       <PaymentLabel>Pagamento</PaymentLabel>
       <InformationContainer>
-        <CardInfoContainer />
-        <FormContainer>
-          <CardNumber placeholder='Card Number' />
-          <CardNumberLabel>E.g.: 49..., 51..., 36..., 37... </CardNumberLabel>
-          <CardOwner placeholder='Name' />
-          <SensitiveCardInfoContainer>
-            <ValidThru placeholder='Valid Thru' />
-            <CVC placeholder='CVC' />
-          </SensitiveCardInfoContainer>
-        </FormContainer>
+        <PaymentForm />
       </InformationContainer>
       <ConfirmButton>Finalizar Pagamento</ConfirmButton>
     </PaymentContainer>
@@ -26,21 +18,5 @@ const PaymentContainer = styled.div``;
 const PaymentLabel = styled.h1``;
 
 const InformationContainer = styled.div``;
-
-const CardInfoContainer = styled.div``;
-
-const FormContainer = styled.div``;
-
-const CardNumber = styled.input``;
-
-const CardNumberLabel = styled.h2``;
-
-const CardOwner = styled.input``;
-
-const SensitiveCardInfoContainer = styled.div``;
-
-const ValidThru = styled.input``;
-
-const CVC = styled.input``;
 
 const ConfirmButton = styled.button``;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,10 @@
+import PaymentGateway from './Components/PaymentGateway';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <PaymentGateway />
+      <h1>Pagamento: Em breve!</h1>
+    </>
+  );
 }
